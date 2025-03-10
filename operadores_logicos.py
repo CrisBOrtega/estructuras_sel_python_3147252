a090aad90acd06aede19c66502f1d5addb182ed3
Your Name <you@example.com>
2025-03-10 07:40:08 -0500

    operador and

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -9,3 +9,7 @@ Acorde a las tablas de Verdad
 #Ejemplo 1: Operador not:
 y = not True
 print("El resultado de operar con not es" ,y)
+
+#ejemplo 2: Operador and
+y = True and True
+print("El resultado de operar con and es" ,y)
